f630262f8adb7ee119b8f99f6d40be34fa24e4ff
axel <axel.wadensten@gmail.com>
2020-09-22 00:13:56 +0200

    Set display variable for jmeter-server

--- elasticsearch-jmeter/jmeter-install.sh
+++ elasticsearch-jmeter/jmeter-install.sh
@@ -104,6 +104,8 @@ create_jmeter_startup_script()
 {
     cat << EOF > /opt/jmeter/start-jmeter-server.sh
 #!/bin/bash
+Xvfb :1 -screen 5 1024x768x8 &
+export DISPLAY=:1.5
 JVM_ARGS="-Xms1024m -Xmx6144m -XX:NewSize=512m -XX:MaxNewSize=6144m" && export JVM_ARGS && /opt/jmeter/apache-jmeter-5.0/bin/jmeter-server -DChrome_Driver=$(which chromedriver) -Djava.rmi.server.hostname=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'`
 EOF
 
@@ -250,7 +252,10 @@ install_chromedriver()
     wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb #TODO: Add this to repo to avoid future version mismatches
 	sudo apt --fix-broken install --assume-yes
 	
-    
+	#Install xvfb (Headless browsers need it too.)
+    sudo apt install xvfb --assume-yes
+	
+	
     #dpkg -i google-chrome*.deb
     #apt-get install -f
     #dpkg -i google-chrome*.deb
