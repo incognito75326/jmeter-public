05e4cdd17115491b26d6cf7363d20c6cf353ac9f
axel <axel.wadensten@avensia.com>
2020-09-23 20:18:28 +0200

    Add tests and upload them to boss node during install

--- elasticsearch-jmeter/jmeter-install.sh
+++ elasticsearch-jmeter/jmeter-install.sh
@@ -187,7 +187,7 @@ install_jmeter()
     wget -O /opt/jmeter/apache-jmeter-5.0/lib/ext/jmeter-plugins-manager-1.3.jar https://jmeter-plugins.org/get/
     
     log "getting test plan"
-    wget -O /opt/jmeter/RNB_POP_preprod_JMeter_ChromeDriver.jmx https://raw.githubusercontent.com/siggeb/jmeter-public/master/elasticsearch-jmeter/resources/RNB_POP_preprod_JMeter_ChromeDriver.jmx
+    wget -O /opt/jmeter/tests.jmx https://raw.githubusercontent.com/axelavensia/jmeter-public/master/elasticsearch-jmeter/resources/tests.jmx
     
     log "installing plugins"
     wget -O /opt/jmeter/apache-jmeter-5.0/lib/cmdrunner-2.2.jar http://search.maven.org/remotecontent?filepath=kg/apc/cmdrunner/2.2/cmdrunner-2.2.jar
